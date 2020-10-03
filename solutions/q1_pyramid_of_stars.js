166201c7da8391fcd6ba13569f315d498f9cb632
Roshan Lal <153064@ust-global.com>
2020-10-03 19:54:05 +0530

    Initial commit

--- solutions/q1_pyramid_of_stars.js
+++ solutions/q1_pyramid_of_stars.js
@@ -1,7 +1,21 @@
 /* Write a program to build a `Pyramid of stars` of given height */
 
 const buildPyramid = () => {
-	// Write your code here
+     // Write your code here
+
+     function pyramid(height){
+          let result = "";
+          for(let i=1;i <= height; i++){
+               for(let k=0;k<height - i;k++){
+                    result += " ";
+               }
+               for(let j=1;j < i; j++){
+                    result += "* ";
+               }
+               result += "*\n";
+          }
+          return result;
+     }
 };
 
 /* For example,
@@ -16,4 +30,4 @@ OUTPUT -
 
 */
 
-module.exports = buildPyramid;
+// module.exports = buildPyramid;
