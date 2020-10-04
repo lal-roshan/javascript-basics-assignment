a66eaa528b4eae9d971bff685d0d854f7a32a39a
Roshan Lal <153064@ust-global.com>
2020-10-04 22:03:51 +0530

    Q4 initial commit

--- solutions/q4_data_modelling_and_retrieval.js
+++ solutions/q4_data_modelling_and_retrieval.js
@@ -6,12 +6,28 @@
 // Write your code here
 
 let fruitsList = [];
-fruitsList.push({ name: "apple", color: "red", pricePerKg: 120.00});
-fruitsList.push({ name: "apple", color: "green", pricePerKg: 180.00});
-fruitsList.push({ name: "orange", color: "orange", pricePerKg: 100.00});
-fruitsList.push({ name: "grapes", color: "green", pricePerKg: 110.00});
-fruitsList.push({ name: "grapes", color: "blue", pricePerKg: 90.00});
-fruitsList.push({ name: "grapes", color: "red", pricePerKg: 100.00});
+fruitsList.push({ name: "apple", color: "red", pricePerKg: 120.00 });
+fruitsList.push({ name: "apple", color: "green", pricePerKg: 180.00 });
+fruitsList.push({ name: "orange", color: "orange", pricePerKg: 100.00 });
+fruitsList.push({ name: "grapes", color: "green", pricePerKg: 110.00 });
+fruitsList.push({ name: "grapes", color: "blue", pricePerKg: 90.00 });
+fruitsList.push({ name: "grapes", color: "red", pricePerKg: 100.00 });
 
-// let fruits = fruitsList.reduce((result, item) => result[]);
+let fruits = {};
+
+for (let i = 0; i < fruitsList.length; i++) {
+    let newData = { color: fruitsList[i]["color"], pricePerKg: fruitsList[i]["pricePerKg"] };
+    let fruitsItem = fruits[fruitsList[i]["name"]];
+    if (fruitsItem) {
+        fruitsItem.push(newData);
+    }
+    else {
+        fruitsItem = new Array(newData);
+    }
+    fruits[fruitsList[i]["name"]] = fruitsItem;
+}
+
+console.log(fruits.apple);
+console.log(fruits.orange);
+console.log(fruits.grapes);
 
