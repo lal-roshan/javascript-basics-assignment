295c23c09fe6ddf5f262c93c6230df813083a670
Roshan Lal <153064@ust-global.com>
2020-10-05 10:45:16 +0530

    Q2 simplified

--- solutions/q2_array_to_object_converter.js
+++ solutions/q2_array_to_object_converter.js
@@ -1,27 +1,25 @@
 /* Write a Program to convert an array of objects to an object
-	based on a given key */
+    based on a given key */
 
 
 const convert = (objArray, field) => {
-	//performs the object creation only if the objArray parameter is array
-	//and if field is a valid string
-	if(Array.isArray(objArray) && field){
+    //performs the object creation only if the objArray parameter is array
+    //and if field is a valid string
+    if(Array.isArray(objArray) && field){
 
-		//return null if the mentioned field is not in the object
-		if(!objArray[0][field]){
-			return null;
-		}
+        //return null if the mentioned field is not in the object
+        if(!objArray[0][field]){
+            return null;
+        }
 
-		let result = {};
+        let result = {};
 
-		//create a result object with provided field as key and the object as value	
-		for(let i=0; i < objArray.length; i++){
-			result[objArray[i][field]] = objArray[i];
-		}
+        //create a result object with provided field as key and the object as value	
+        objArray.forEach((item) => result[item[field]] = item);
 
-		return result;
-	}
-	return null;
+        return result;
+    }
+    return null;
 };
 
 module.exports = convert;
